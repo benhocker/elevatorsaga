aabfeffd8f86245e2039b89f8669ae625355eb3d
Ben Hocker <ben.hocker@cat.com>
2015-02-12 17:48:46 -0600

    Beat Level 1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
             // The elevator is idle, so let's go to all the floors (or did we forget one?)
             elevator.goToFloor(0);
             elevator.goToFloor(1);
+            elevator.goToFloor(2);
         });
     },
     update: function(dt, elevators, floors) {
